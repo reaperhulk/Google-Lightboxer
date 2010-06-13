72979db20a37fe2769a46a0e566ac52210468c34
Paul Kehrer <pkehrer@hyperion.(none)>
2010-06-13 18:34:36 -0500

    fixing issues 1 and 3 on github

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,9 @@
-$(window).load(function() {
+/*
+to capture and re-colorbox when sizes and types are changed on the left sidebar we need to bind a live 
+event. i'd rather watch for div content changes but i'm not sure how to do that at this time.
+*/
+
+triggerColorBox = function() {
 	$('#ImgContent a > img').parent().colorbox({
 		href:function() {
 			href = $(this).attr('href');
@@ -20,4 +25,23 @@ $(window).load(function() {
 		minWidth:'200',
 		minHeight:'200'
 	})
+}
+
+$(window).load(function() {
+	$('#ImgContent a').live('mouseover',function(event) {
+		if(event.metaKey) {
+			event.stopPropagation();
+			$(this).removeClass('cboxElement');
+		} else {
+			triggerColorBox();
+		}
+		return false;
+	})
+		$('#ImgContent a').live('click',
+		function(event) {
+			if(event.metaKey) {
+				event.stopPropagation();
+				triggerColorBox()
+			}
+		})
 })
